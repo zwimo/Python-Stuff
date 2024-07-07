69427333945572a75ec9a432fc95835f6e889e25
Zachary Mollett <zwimox@gmail.com>
2024-07-07 16:34:43 -0500

    Add files via upload

---
+++ number_guesser.py
@@ -0,0 +1,33 @@
+import random
+
+top_range = input("Please type a number: ")
+
+if top_range.isdigit():
+    top_range = int(top_range)
+
+    if top_range <= 0:
+        print('Try a number greater than zero next time.')
+        quit()
+else:
+    print('You need to type a number next time.')
+random_number = random.randint(0, top_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('You need to type a number next time.')   
+        continue
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess > random_number:
+            print ("Maybe try a lower number.")
+    else:
+            print("Try a higher number.")
+
+print("You guessed", guesses, "times.")
